84dac6c03648c781124411e6212e200be0b2da5e
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-01-08 11:59:06 +0300

    section9 complate

---
+++ Strings/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Strings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sentence = "My name is Engin Demiroğ";
+        var result = sentence.Length;
+        var result2 = sentence.Clone();
+        sentence = "My name is Derin Demiroğ";
+        
+        bool result3 = sentence.EndsWith("ğ");
+        bool result4 = sentence.StartsWith("My name");
+
+        var result5 = sentence.IndexOf("name");
+        var result6 = sentence.IndexOf(" ");
+        var result7 = sentence.LastIndexOf(" ");
+        var result8 = sentence.Insert(0, "Hello, ");
+        var result9 = sentence.Substring(3,4);
+        var result10 = sentence.ToLower();
+        var result11 = sentence.ToUpper();
+        var result12 = sentence.Replace(" ", "-");
+        var result13 = sentence.Remove(2, 4);
+
+
+        Console.WriteLine(result2 +  result13);
+    }
+    private static void Intro()
+    {
+        string city = "Ankara";
+        foreach (var item in city)
+        {
+            Console.WriteLine(item);
+        }
+
+        //string city2 = city + "İstanbul";
+        string city2 = "İstanbul";
+        
+        Console.WriteLine(String.Format("{0} {1}", city, city2));
+    }
+}
+
