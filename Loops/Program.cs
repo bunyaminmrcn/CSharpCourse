5393bab8ec76aeb56ae91c2a9815f7e6ad7cb6a1
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-01-08 11:19:07 +0300

    section8 complate

---
+++ Loops/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //FoorLoop();
+        //WhileLoop();
+        //DoWhileLoop();
+        //ForEachLoop();
+        Console.WriteLine(IsPrimeNumber(6));
+    }
+
+
+    private static bool IsPrimeNumber(int number)
+    {
+        bool result = true;
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                result = false;
+                i = number;  //do this trick
+                //or use break,continue;
+            }
+        }
+        return result;
+    }
+    private static void ForEachLoop()
+    {
+        string[] students = new string[3] { "Engin", "Derin", "Salih" };
+
+        foreach (var student in students)
+        {
+            Console.WriteLine(student);
+        }
+    }
+    private static void DoWhileLoop()
+    {
+        int number = 10;
+        do
+        {
+            Console.WriteLine(number);
+            number--;
+        } while (number >= 0);
+    }
+    private static void WhileLoop()
+    {
+        int number = 100;
+        while (number <= 0)
+        {
+            Console.WriteLine(number);
+            number--;
+        }
+    }
+    private static void FoorLoop()
+    {
+        for (int i = 100; i >= 0; i -= 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
