f8723216cbd5767b01ef90917724f57a56051902
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-01-08 12:32:51 +0300

    section10 complate

---
+++ Classes/ProductManager.cs
@@ -0,0 +1,12 @@
+namespace Classes;
+class ProductManager {
+    public void Add() 
+    {
+        Console.WriteLine("Product Added!");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Product Updated!");
+    }
+}
