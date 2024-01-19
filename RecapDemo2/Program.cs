f8030870db368ed3f92072c58d7e20178b11da68
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-01-19 20:18:25 +0300

    section15 complete

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace RecapDemo2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.logger = new DatabaseLogger();
+        customerManager.Add();
+    }
+}
+
+class CustomerManager
+{
+    public ILogger logger { get; set; }
+    public void Add()
+    {
+        logger.Log();
+        Console.WriteLine("Customer added!");
+    }
+}
+
+class DatabaseLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to database!");
+    }
+}
+
+class FileLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to file!");
+    }
+}
+
+class SmsLogger : ILogger
+{
+    public void  Log()
+    {
+        Console.WriteLine("Logged to sms!");
+    }
+}
+interface ILogger 
+{
+    void Log();
+}
