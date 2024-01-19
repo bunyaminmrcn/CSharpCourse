c9664c10fc98d34399fb7df63024c80b52407358
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-01-19 17:57:58 +0300

    section14 complete

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace AbstractClasses;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Database database = new Oracle();
+        database.Add();
+        database.Delete();
+
+        Database database2 =new SqlServer();
+        database2.Add();
+        database2.Delete();
+    }
+}
+
+//You cant use Database as instance
+abstract class Database 
+{
+    public void Add() 
+    {
+        Console.WriteLine("Added by default");
+    }
+
+    public abstract void Delete();
+}
+
+class SqlServer : Database
+{
+    public override void Delete()
+    {
+        Console.WriteLine("Deleted by Sql");
+    }
+}
+
+class Oracle : Database
+{
+    public override void Delete()
+    {
+        Console.WriteLine("Deleted by Oracle");
+    }
+}
