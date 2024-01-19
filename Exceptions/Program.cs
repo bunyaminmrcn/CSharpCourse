ebcdec7627148477ad35b65facbcc36cc602b3c2
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-01-19 23:49:06 +0300

    section20 complete

---
+++ Exceptions/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Exceptions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Find();
+        }
+        catch (RecordNotFoundException exception)
+        {
+            Console.WriteLine(exception.Message);
+
+        }
+
+        HandleException(() =>
+        {
+            Find();
+        });
+    }
+
+    private static void HandleException(Action action)
+    {
+        try
+        {
+            action.Invoke();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+
+        }
+    }
+
+    private static void Find()
+    {
+        List<string> students = new List<string> { "Engin", "Derin", "Salih" };
+        if (!students.Contains("Ahmet"))
+        {
+            throw new RecordNotFoundException("Record Not Found!");
+        }
+    }
+    private static void ExceptionIntro()
+    {
+        try
+        {
+            string[] students = new string[] { "Engin", "Derin", "Salih" };
+            students[3] = "Ahmet";
+        }
+        catch (IndexOutOfRangeException exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+
+        catch (DivideByZeroException exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
